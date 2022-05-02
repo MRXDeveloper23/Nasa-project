e2650334bd42b799c31d31be851423ab4c7caefc
Khurmatillo <72973599+MRXDeveloper23@users.noreply.github.com>
2022-05-02 21:32:51 +0500

    mongodb launches schema added

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,32 @@
 const http = require("http");
 const app = require("./app");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const {
   loadPlanetsData,
 } = require("./models/planets.model");
+const MONGO_URL =
+  "mongodb+srv://developer:developer23@cluster0.pdevd.mongodb.net/nasaProject?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 
-(async function () {
-  await loadPlanetsData();
-})();
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready");
 });
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
+startServer();
